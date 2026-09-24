c79cbd720b69627c05cb6e76159b547b61cbd185
berrettwc-byte <berrettwc@gmail.com>
2026-09-24 11:48:52 -0600

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        bool done = false;
+
+        while (!done)
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guesses = int.Parse(guess);
+            
+            if (guesses > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guesses < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.Write("Are you done (y/n)? ");
+                done = Console.ReadLine().ToLower() == "y";
+            }
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,25 +1,68 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int x = 10;
-        int y = 30;
-        int z = 40;
+        // int x = 10;
+        // int y = 30;
+        // int z = 40;
 
-        if (x == 10 && y == 30 || z == 30) /* && is and || is or */
-        {
-            Console.WriteLine("X is 10!");
-            Console.WriteLine("Y is fun!");
-        }
-        else if (x == 20)
-        {
-            Console.WriteLine("X is 20");
-        }
-        else
-        {
-            Console.WriteLine("Default output");
-        }
+        // if (x == 10 && y == 30 || z == 30) /* && is and || is or */
+        // {
+        //     Console.WriteLine("X is 10!");
+        //     Console.WriteLine("Y is fun!");
+        // }
+        // else if (x == 20)
+        // {
+        //     Console.WriteLine("X is 20");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Default output");
+        // }
+
+
+
+
+        // bool done = false;
+
+        // while (!done)
+        // {
+        //     Console.Write("Are we done yet (y/n)? ");
+        //     done = Console.ReadLine() == "y";
+        // }
+
+
+
+
+        // bool done = false;
+
+        // do
+        // {
+        //     Console.Write("Are we done yet (y/n)? ");
+        //     done = Console.ReadLine().ToLower() == "y";
+        // }while (!done);
+
+
+
+        
+        // for(int i = 100; i >= -50; i -= 10)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+
+
+
+        // List<string> myFriends = new List<string> = ["Bob", "Betty", "Bubba"];
+
+        // myFriends.Add("doug");
+
+        // foreach(string friend in myFriends)
+        // {
+        //     Console.WriteLine(friend);
+        // }
     }
 }
